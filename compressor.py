bd101efb9d09fb600b80642ad03196c440de5529
cattywat <cattywat3@gmail.com>
2025-04-03 08:45:46 +0100

    still gets stuck at length 1

--- compressor.py
+++ compressor.py
@@ -8,28 +8,31 @@ while(len(a)>0):
     print(len(a))
     i=0
     while i<len(a):
-        if(a[0:-i] in dictionary):
-            for k in range(len(dictionary)):
-                if(dictionary[k]==a[0:-i]):
-                    a=a[-i:]
-                    outfile.write(str(k)+' ')
-                    break
-            print('something has definitely gone wrong')
+        found=False
+        for k in range(len(dictionary)):
+            if(dictionary[k]==a[0:-i]):
+                found=True
+                a=a[-i:]
+                outfile.write(str(k)+' ')
+                break
         i+=1
-    maxlen=0
-    total=0
-    for j in range(len(a)):
-        if(total>=0.99*len(a)):
-            break
-        elif(j*a.count(a[0:j])>total):
-            maxlen=j
-            total=j*a.count(a[0:j])
-        elif(a.count(a[0:j])==1):
-            break
-    dictionary.append(a[0:maxlen])
-    print(dictionary)
-    outfile.write(str(len(dictionary)-1))
-    a=a[maxlen:]
+    if not found:
+        maxlen=0
+        total=0
+        for j in range(len(a)):
+            if(j==1):
+                continue
+            elif(total>=0.99*len(a)):
+                break
+            elif(j*a.count(a[0:j])>total):
+                maxlen=j
+                total=j*a.count(a[0:j])
+            elif(a.count(a[0:j])==1):
+                break
+        dictionary.append(a[0:maxlen])
+        print(dictionary)
+        outfile.write(str(len(dictionary)-1))
+        a=a[maxlen:]
 
 outfile.write('\n')
 for i in range(len(dictionary)):
