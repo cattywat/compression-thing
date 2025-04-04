1c7dbc570486baed20ea4fccb5a10fb92d509502
cattywat <cattywat3@gmail.com>
2025-04-05 00:29:34 +0100

    still isnt working and i havent the foggiest why

--- compressor.py
+++ compressor.py
@@ -4,17 +4,19 @@ dictionary=[]
 outfile=open(directory+'compressed','w',encoding='utf8')
 a=file.read()
 
-while(len(a)>0):
-    i=0
-    while i<len(a):
-        found=False
-        for k in range(len(dictionary)):
-            if(dictionary[k]==a[0:-i]):
-                found=True
-                a=a[-i:]
-                outfile.write(str(k)+' ')
-                break
-        i+=1
+while(len(a)>1):
+    found=False
+    for i in range(len(a)):
+        if not (a[0:int((len(a)-i)/2)] in dictionary):
+            break
+        elif(a[0:-i] in dictionary):
+            print(i)
+            for k in range(len(dictionary)):
+                if(dictionary[k]==a[0:-i]):
+                    found=True
+                    a=a[-i:]
+                    outfile.write(str(k)+' ')
+                    break
     if not found:
         maxlen=0
         total=0
@@ -29,8 +31,21 @@ while(len(a)>0):
             elif(a.count(a[0:j])==1):
                 break
         dictionary.append(a[0:maxlen])
-        outfile.write(str(len(dictionary)-1))
+        outfile.write(str(len(dictionary)-1)+' ')
         a=a[maxlen:]
+    print(len(a))
+    print(dictionary[-1])
+if(len(a)==1):
+    if(a in dictionary):
+        for k in range(len(dictionary)):
+            if(dictionary[k]==a):
+                a=a[-i:]
+                outfile.write(str(k)+' ')
+                break
+        i+=1
+    if not found:
+        dictionary.append(a)
+        outfile.write(str(len(dictionary)-1)+' ')
 
 outfile.write('\n')
 for i in range(len(dictionary)):
