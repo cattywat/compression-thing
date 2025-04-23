39cf34e11eaff88026fa0870e7babff8dba12494
cattywat <cattywat3@gmail.com>
2025-04-23 10:33:47 +0100

    added frequency analyser for text

---
+++ frequencyAnalyser.py
@@ -0,0 +1,60 @@
+def sort(toSort):
+    for i in range(len(toSort)-1):
+        swapped=False
+        for j in range(len(toSort)-1-i):
+            if(toSort[j][1]>toSort[j+1][1]):
+                temp=toSort[j]
+                toSort[j]=toSort[j+1]
+                toSort[j+1]=temp
+                swapped=True
+        if(not swapped):
+            return toSort
+    return toSort
+def breakdown(tree):
+    if(type(tree[0])==str and type(tree[1])==str):
+        return {0:tree[0],1:tree[1]}
+    left=tree[0]
+    right=tree[1]
+    if(len(left)==1):
+        left={0:left}
+    else:
+        left=breakdown(left)
+    if(len(right)==1):
+        right={0:right}
+    else:
+        right=breakdown(right)
+    result={}
+    for key,value in left.items():
+        result.update({('0'+str(key)):value})
+    for key,value in right.items():
+        result.update({('1'+str(key)):value})
+    return result
+
+
+
+file=open(input(),'r',encoding='utf8')
+char=file.read(1)
+dictionary=[[char,1]]
+
+for a in range(10000):
+    char=file.read(1)
+    print(a)
+    found=False
+    for i in range(len(dictionary)):
+        if(dictionary[i][0]==char):
+            dictionary[i][1]+=1
+            found=True
+    if not found:
+        dictionary.append([char,1])
+
+tree=sort(dictionary)
+
+while(len(tree)>1):
+    print(len(tree))
+    a=tree.pop(0)
+    b=tree[0]
+    tree[0]=[[a[0],b[0]],a[1]+b[1]]
+    tree=sort(tree)
+
+final=breakdown(tree[0][0])
+print(final)
