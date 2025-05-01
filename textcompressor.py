69d057b1a4a2f32210182b3d5ec896eb6a071af6
cattywat <130406611+cattywat@users.noreply.github.com>
2025-05-01 09:33:07 +0100

    fixed bits being bytes

--- textcompressor.py
+++ textcompressor.py
@@ -17,7 +17,7 @@ for i in range(length-5):
         print(encoder[c])
         print(int(encoder[c],2))
         print(int(encoder[c],2).to_bytes(math.ceil(len(encoder[c])/8)))
-        final.write((int(encoder[c],2)).to_bytes(len(encoder[c])))
+        final.write((int(encoder[c],2)).to_bytes(math.ceil(len(encoder[c])/8)))
     except KeyError as e:
         print(e.args[0])
     c=file.read(1)
